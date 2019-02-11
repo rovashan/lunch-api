7b6fb3f0f3e3ee5b07ba2fb184f912d9701c1a25
Rovashan <rovashan@gmail.com>
2019-02-11 12:47:44 +0200

    Add express api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from "body-parser";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+const main = express();
+
+main.use(cors());
+main.use('/api', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+// webApi is your functions name, and you will pass main as 
+// a parameter
+export const webApi = functions.https.onRequest(main);
+
+// api functions
+app.get('/hello', (req, res) => {
+    res.send('hello');
+});
+
